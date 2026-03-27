c7ba8039ed86bb6169f552a1cea6ad946ad92a84
Minushii <minushidesilva6@gmail.com>
2026-03-27 12:28:27 +1100

    Initial project setup with folders and venv

---
+++ main.py
@@ -0,0 +1,11 @@
+""" pip install pandas -> 
+                openpyxl ->library in Python is a powerful tool for reading, writing, and modifying Excel 2010 and later files
+                matplotlib -> visualization in python 
+                seaborn -> used for creating graphics
+
+"""
+
+import pandas as pd
+
+#load the prompts 
+prompts = pd.read_
